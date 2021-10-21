3761341909b8b8c858ab4febb89b80de6fa9f6f2
Huttemeitu <83450937+Huttemeitu@users.noreply.github.com>
2021-10-21 11:49:03 +0200

    Add files via upload

---
+++ IN1010 - Java/Oblig1/BilBruk2.java
@@ -0,0 +1,8 @@
+public class BilBruk2 {
+
+    public static void main(String[]args) {
+        Bil2 bil2 = new Bil2("DN73502");
+        bil2.skrivUt2();
+    }
+    
+}
---
+++ IN1010 - Java/Oblig1/BilBruk3.java
@@ -0,0 +1,11 @@
+public class BilBruk3 {
+    Person pers;
+    public static void main(String[]args) {
+        Bil3 nybil = new Bil3("AY16416");
+        Person pers = new Person(nybil);
+
+        pers.skrivUt4();
+
+
+    }
+}
