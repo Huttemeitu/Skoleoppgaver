0b238631343b918e78de7450054907f7bb78d0ae
Huttemeitu <83450937+Huttemeitu@users.noreply.github.com>
2021-10-21 12:23:59 +0200

    Add files via upload

---
+++ IN1010 - Java/Oblig7/Tuppel (2).java
@@ -0,0 +1,14 @@
+public class Tuppel {
+    int rad; 
+    int kolonne; 
+
+    public Tuppel(int rad, int kolonne) {
+        this.rad = rad; 
+        this.kolonne = kolonne; 
+    }
+
+    public String toString() {
+        return "Rad: " + rad + " Kolonne: " + kolonne;
+    }
+    
+}
